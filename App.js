aab301c2f512512eb47213984b8a9d1e349d5c62
Alejo Costa <alejocosta50@hotmail.com>
2023-09-23 17:23:45 -0300

    Version 1.0

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import { View } from 'react-native';
-import Login from './screens/Login';
+import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import BackgroundImage from './components/backgroundImage';
+import Profile from './components/perfil';
 
 export default function App() {
   return (
-    <Login />
+    <SafeAreaView style={styles.container}>
+      <BackgroundImage>
+        <Profile />
+      </BackgroundImage>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
