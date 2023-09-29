9c3bf3a0cfe6e66e860c306c555c0be055f33f71
FrancoBorsella1 <fborsella1@gmail.com>
2023-09-29 14:04:58 -0300

    Se renderizó la pantalla Actividad

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import BackgroundImage from './components/backgroundImage';
-import Profile from './components/perfil';
+//import BackgroundImage from './components/backgroundImage';
+//import Profile from './screens/perfil';
+import Actividad from './screens/Actividad';
+//import Login from './screens/Login';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <BackgroundImage>
-        <Profile />
-      </BackgroundImage>
-      <StatusBar style="auto" />
+      <Actividad/>
     </SafeAreaView>
   );
 }
@@ -21,3 +20,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+//Probar pantalla perfil
+/*
+ return (
+    <SafeAreaView style={styles.container}>
+      <BackgroundImage>
+        <Profile />
+      </BackgroundImage>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  ); 
+*/
